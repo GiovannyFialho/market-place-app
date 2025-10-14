656f92a4cad6a68a8894057bd1db7e2278996369
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2025-10-14 19:28:23 -0300

    installing nativewind

--- mktplace-app/src/app/index.tsx
+++ mktplace-app/src/app/index.tsx
@@ -4,7 +4,7 @@ import { Text, TouchableOpacity, View } from "react-native";
 export default function App() {
   return (
     <View>
-      <Text>Hello World</Text>
+      <Text className="text-purple-base text-lg font-bold">Hello World</Text>
 
       <TouchableOpacity onPress={() => router.push("login")}>
         <Text>Go to login</Text>
