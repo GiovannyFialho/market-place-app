f9639269414d627cc9fae3fcf16b3ec0115b7e45
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2025-10-15 18:51:41 -0300

    creating public routes

--- mktplace-app/src/app/index.tsx
+++ mktplace-app/src/app/index.tsx
@@ -1,14 +1,5 @@
-import { router } from "expo-router";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Redirect } from "expo-router";
 
 export default function App() {
-  return (
-    <View>
-      <Text className="text-purple-base text-lg font-bold">Hello World</Text>
-
-      <TouchableOpacity onPress={() => router.push("login")}>
-        <Text>Go to login</Text>
-      </TouchableOpacity>
-    </View>
-  );
+  return <Redirect href="/login" />;
 }
