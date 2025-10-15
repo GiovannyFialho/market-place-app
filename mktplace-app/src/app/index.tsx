929c3db83af9e441ef0556bcafc956643db8669b
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2025-10-15 19:04:48 -0300

    creating private routes

--- mktplace-app/src/app/index.tsx
+++ mktplace-app/src/app/index.tsx
@@ -1,5 +1,13 @@
 import { Redirect } from "expo-router";
 
 export default function App() {
+  const userData = {
+    token: "iej21i2j2dd",
+  };
+
+  if (!userData) {
+    return <Redirect href="/(private)/home" />;
+  }
+
   return <Redirect href="/login" />;
 }
