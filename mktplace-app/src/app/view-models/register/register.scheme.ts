20162ee41495ac4b1deee760838c77ce37c745ea
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2025-10-20 21:24:41 -0300

    creating schema user register

---
+++ mktplace-app/src/app/view-models/register/register.scheme.ts
@@ -0,0 +1,34 @@
+import { z } from "zod";
+
+export const registerSchema = z
+  .object({
+    name: z
+      .string()
+      .min(4, { message: "Nome deve ter pelo menos 4 caracteres" })
+      .nonempty({ message: "Nome é obrigatório" }),
+    email: z
+      .email({ message: "E-mail inválido" })
+      .nonempty({ message: "E-mail é obrigatório" }),
+    password: z
+      .string()
+      .min(6, { message: "Senha deve ter pelo menos 6 caracteres" })
+      .nonempty({ message: "Senha é obrigatória" }),
+    confirmPassword: z.string().nonempty({ message: "Senha é obrigatória" }),
+    phone: z
+      .string()
+      .regex(/^\d{11}$/, {
+        message: "Telefone deve ter 11 dígitos (DDD + número)",
+      })
+      .nonempty({ message: "Telefone é obrigatório" }),
+  })
+  .superRefine(({ password, confirmPassword }, ctx) => {
+    if (password !== confirmPassword) {
+      ctx.addIssue({
+        code: "custom",
+        message: "Senhas não coincidem",
+        path: ["confirmPassword"],
+      });
+    }
+  });
+
+export type RegisterSchema = z.infer<typeof registerSchema>;
