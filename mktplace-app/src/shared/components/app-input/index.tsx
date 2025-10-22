ea3e452cba63ca9628d0996dac5a2c1a10938849
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2025-10-22 18:13:00 -0300

    adjusting app-input component function

--- mktplace-app/src/shared/components/app-input/index.tsx
+++ mktplace-app/src/shared/components/app-input/index.tsx
@@ -3,7 +3,7 @@ import { Pressable, TextInput, TouchableOpacity, View } from "react-native";
 
 import { appInputVariants } from "./input.variants";
 
-export const AppInput = () => {
+export function AppInput() {
   const styles = appInputVariants();
 
   return (
@@ -19,4 +19,4 @@ export const AppInput = () => {
       </Pressable>
     </View>
   );
-};
+}
