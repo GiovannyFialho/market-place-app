b0d02585943285a9875583e12864ab9305a72e99
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2025-11-04 20:31:01 -0300

    creating keyboard-container component

--- mktplace-app/src/view-models/sign-in/sign-in.view.tsx
+++ mktplace-app/src/view-models/sign-in/sign-in.view.tsx
@@ -3,21 +3,24 @@ import { Text, TouchableOpacity, View } from "react-native";
 
 import { AppInput } from "../../shared/components/app-input";
 import { AuthFormHeader } from "../../shared/components/auth-form-header";
+import { KeyboardContainer } from "../../shared/components/keyboard-container";
 
 export function SignInView() {
   return (
-    <View className="items-center justify-center flex-1">
-      <AuthFormHeader
-        subTitle="Informe seu e-mail e senha"
-        title="Acesse sua conta"
-      />
+    <KeyboardContainer>
+      <View className="items-center justify-center flex-1 px-[40px]">
+        <AuthFormHeader
+          subTitle="Informe seu e-mail e senha"
+          title="Acesse sua conta"
+        />
 
-      <AppInput label="E-mail" leftIcon="mail-outline" />
-      <AppInput label="Senha" leftIcon="lock-closed-outline" />
+        <AppInput label="E-mail" leftIcon="mail-outline" />
+        <AppInput label="Senha" leftIcon="lock-closed-outline" />
 
-      <TouchableOpacity onPress={() => router.push("/sign-up")}>
-        <Text>Registro</Text>
-      </TouchableOpacity>
-    </View>
+        <TouchableOpacity onPress={() => router.push("/sign-up")}>
+          <Text>Registro</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardContainer>
   );
 }
--- mktplace-app/src/view-models/sign-up/sign-up.view.tsx
+++ mktplace-app/src/view-models/sign-up/sign-up.view.tsx
@@ -1,41 +1,73 @@
 import { router } from "expo-router";
-import { Text, TouchableOpacity, View } from "react-native";
+import { ScrollView, Text, TouchableOpacity } from "react-native";
 
 import { useSignUpSchemaViewModel } from "./useSignUp.viewModel";
 
 import { AppInputController } from "../../shared/components/app-input-controller";
 import { AuthFormHeader } from "../../shared/components/auth-form-header";
+import { KeyboardContainer } from "../../shared/components/keyboard-container";
 
 type SignUpViewProps = ReturnType<typeof useSignUpSchemaViewModel>;
 
 export default function SignUpView({ control, onSubmit }: SignUpViewProps) {
   return (
-    <View className="flex-1 items-center justify-center">
-      <AuthFormHeader
-        title="Crie sua conta"
-        subTitle="Informe seus dados pessoais e de acesso"
-      />
+    <KeyboardContainer>
+      <ScrollView className="flex-1 px-[40px]">
+        <AuthFormHeader
+          title="Crie sua conta"
+          subTitle="Informe seus dados pessoais e de acesso"
+        />
 
-      <AppInputController
-        control={control}
-        name="email"
-        label="E-MAIL"
-        leftIcon="mail-outline"
-      />
+        <AppInputController
+          leftIcon="person-outline"
+          label="NOME"
+          control={control}
+          name="name"
+        />
 
-      <TouchableOpacity
-        className="mt-5 bg-slate-900 w-max py-2 px-3 rounded-lg"
-        onPress={onSubmit}
-      >
-        <Text className="text-lg font-bold text-white">Registrar</Text>
-      </TouchableOpacity>
+        <AppInputController
+          leftIcon="call-outline"
+          label="TELEFONE"
+          control={control}
+          name="phone"
+        />
+        <AppInputController
+          leftIcon="mail-outline"
+          label="E-MAIL"
+          control={control}
+          name="email"
+        />
 
-      <TouchableOpacity
-        className="mt-5"
-        onPress={() => router.push("/sign-in")}
-      >
-        <Text>Login</Text>
-      </TouchableOpacity>
-    </View>
+        <AppInputController
+          leftIcon="lock-closed-outline"
+          label="SENHA"
+          control={control}
+          name="password"
+          secureTextEntry
+        />
+
+        <AppInputController
+          leftIcon="lock-closed-outline"
+          label="CONFIRMAR SENHA"
+          control={control}
+          name="confirmPassword"
+          secureTextEntry
+        />
+
+        <TouchableOpacity
+          className="mt-5 bg-slate-900 w-max py-2 px-3 rounded-lg"
+          onPress={onSubmit}
+        >
+          <Text className="text-lg font-bold text-white">Registrar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          className="mt-5"
+          onPress={() => router.push("/sign-in")}
+        >
+          <Text>Login</Text>
+        </TouchableOpacity>
+      </ScrollView>
+    </KeyboardContainer>
   );
 }
