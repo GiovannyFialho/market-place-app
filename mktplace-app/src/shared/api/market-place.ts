c55745391826fed191aa9d40073ac8f2651346fc
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2025-12-16 17:48:41 -0300

    adjusting refreshToken

--- mktplace-app/src/shared/api/market-place.ts
+++ mktplace-app/src/shared/api/market-place.ts
@@ -1,5 +1,6 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios, { type AxiosInstance } from "axios";
+import { useUserStore } from "../store/user-store";
 
 export const baseURL = "http://192.168.68.104:3001";
 
@@ -40,6 +41,82 @@ export class MarketPlaceAPIClient {
         return Promise.reject(error);
       }
     );
+
+    this.instance.interceptors.response.use(
+      (response) => response,
+      async (error) => {
+        alert("Caiu no refresh");
+
+        const originalRequest = error.config;
+
+        if (
+          error.response?.status === 401 &&
+          error.response?.data?.message === "Token expirado" &&
+          !this.isRefreshing
+        ) {
+          this.isRefreshing = true;
+
+          try {
+            const userData = await AsyncStorage.getItem("marketplace-auth");
+
+            if (!userData) {
+              throw new Error("Usuário não autenticado!");
+            }
+
+            const {
+              state: { refreshToken },
+            } = JSON.parse(userData);
+
+            if (!refreshToken) {
+              throw new Error("Refresh Token não encontrado");
+            }
+
+            const { data: response } = await this.instance.post(
+              "/auth/refresh",
+              {
+                refreshToken,
+              }
+            );
+
+            const currentUserData = JSON.parse(userData);
+
+            currentUserData.state.token = response.token;
+            currentUserData.state.refreshToken = response.refreshToken;
+
+            await AsyncStorage.setItem(
+              "marketplace-auth",
+              JSON.stringify(currentUserData)
+            );
+
+            originalRequest.headers.Authorization = `Bearer ${response.token}`;
+
+            return this.instance(originalRequest);
+          } catch (error) {
+            this.handleUnauthorized();
+
+            return Promise.reject(
+              new Error("Sessão expirada, faça o login novamente")
+            );
+          } finally {
+            this.isRefreshing = false;
+          }
+        }
+
+        if (error.response && error.response.data) {
+          return Promise.reject(new Error(error.response.data.message));
+        } else {
+          return Promise.reject(new Error("Falha na requisição"));
+        }
+      }
+    );
+  }
+
+  private async handleUnauthorized() {
+    const { logout } = useUserStore.getState();
+
+    delete this.instance.defaults.headers.common["Authorization"];
+
+    logout();
   }
 }
 
