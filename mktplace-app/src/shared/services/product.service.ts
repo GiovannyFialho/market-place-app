9911bd42588f9ed6ba39813c3d8b0d99ae009676
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2025-12-29 18:41:13 -0300

    creating products infinity query

--- mktplace-app/src/shared/services/product.service.ts
+++ mktplace-app/src/shared/services/product.service.ts
@@ -1,24 +1,7 @@
 import { marketPlaceAPIClient } from "../api/market-place";
+import { ProductRequest } from "../interfaces/http/product";
 
 import type { ProductResponse } from "../interfaces/http/product-response";
-
-interface ProductRequest {
-  pagination: {
-    page: number;
-    perPage: number;
-  };
-  filters: {
-    from: Date;
-    to: Date;
-    categoryIds: number[];
-    searchText: string;
-    minValue: number;
-    maxValue: number;
-  };
-  sort: {
-    averageRating: string;
-  };
-}
 
 export const getProducts = async (params: ProductRequest) => {
   const { data } = await marketPlaceAPIClient.get<ProductResponse>(
