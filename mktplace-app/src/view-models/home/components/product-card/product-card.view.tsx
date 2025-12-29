2c1dec3d0e846a938f81d2ebc94f3b5dd09f1efb
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2025-12-29 18:23:02 -0300

    product stylization

--- mktplace-app/src/view-models/home/components/product-card/product-card.view.tsx
+++ mktplace-app/src/view-models/home/components/product-card/product-card.view.tsx
@@ -1,4 +1,7 @@
-import { Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { Image, Text, TouchableOpacity, View } from "react-native";
+
+import { colors } from "../../../../styles/colors";
 
 import { useProductCardViewModel } from "./useProductCard.model";
 
@@ -6,8 +9,32 @@ export function ProductCardView({
   product,
 }: ReturnType<typeof useProductCardViewModel>) {
   return (
-    <View>
-      <Text>{product.name}</Text>
-    </View>
+    <TouchableOpacity className="w-[48%] my-1 rounded-xl shadow-sm overflow-hidden h-[157px] p-[4px] bg-white mb-2">
+      <View>
+        <Image
+          source={{ uri: product.photo }}
+          className="w-full h-[96px] rounded-md"
+          resizeMode="cover"
+        />
+
+        <View className="absolute top-0 right-0 flex-row items-center px-2 py-1 rounded-b-lg rounded-r-none bg-white">
+          <Ionicons name="star" size={12} color={colors["blue-base"]} />
+
+          <Text className="text-sm font-semibold ml-1">
+            {product.ratingCount}
+          </Text>
+        </View>
+      </View>
+
+      <View className="p-3">
+        <Text className="text-xs font-semibold mb-1" numberOfLines={1}>
+          {product.name}
+        </Text>
+
+        <View className="flex-row items-center justify-between">
+          <Text>R$ {product.value}</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
   );
 }
