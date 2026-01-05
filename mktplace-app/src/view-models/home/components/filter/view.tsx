65f8755a58ca2be5d58b971c562ce4d04f4b4c90
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2026-01-05 18:27:30 -0300

    creating filter base to list products'

--- mktplace-app/src/view-models/home/components/filter/view.tsx
+++ mktplace-app/src/view-models/home/components/filter/view.tsx
@@ -1,11 +1,59 @@
-import { Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { Text, TouchableOpacity, View } from "react-native";
+
+import { AppButton } from "../../../../shared/components/app-button";
+import { AppInput } from "../../../../shared/components/app-input";
 
 import { useFilterModel } from "./model";
 
+import { colors } from "../../../../styles/colors";
+
 export function FilterView({}: ReturnType<typeof useFilterModel>) {
   return (
-    <View className="h-72 mb-11">
-      <Text>Hello</Text>
+    <View>
+      <View className="flex-row items-center justify-between py-4 px-6">
+        <Text className="text-lg font-bold text-gray-900">
+          Filtrar anúncios
+        </Text>
+
+        <TouchableOpacity>
+          <Ionicons name="close" size={20} color={colors["purple-base"]} />
+        </TouchableOpacity>
+      </View>
+
+      <View className="py-4 px-6">
+        <Text className="font-semibold text-base text-gray-300">VALOR</Text>
+
+        <View className="flex-row mb-4 w-full">
+          <View className="flex-1">
+            <AppInput
+              placeholder="De"
+              keyboardType="numeric"
+              containerClassName="w-[90%]"
+            />
+          </View>
+
+          <View className="flex-1">
+            <AppInput
+              placeholder="Até"
+              keyboardType="numeric"
+              containerClassName="w-[90%]"
+            />
+          </View>
+        </View>
+
+        <Text className="font-semibold text-base text-gray-300">CATEGORIA</Text>
+
+        <View className="flex-row gap-3 mt-4 mb-6">
+          <View className="flex-1">
+            <AppButton variant="outlined">Limpar filtro</AppButton>
+          </View>
+
+          <View className="flex-1">
+            <AppButton>Filtrar</AppButton>
+          </View>
+        </View>
+      </View>
     </View>
   );
 }
