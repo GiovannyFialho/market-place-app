43913f561306da8a8854decbbd02fabe4eee7f03
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2026-01-09 17:20:25 -0300

    improving the style of the product card

--- mktplace-app/src/view-models/home/components/product-card/useProductCard.model.ts
+++ mktplace-app/src/view-models/home/components/product-card/useProductCard.model.ts
@@ -7,7 +7,20 @@ interface UseProductCardViewModelParams {
 export function useProductCardViewModel({
   product,
 }: UseProductCardViewModelParams) {
+  function formatProductName(name: string) {
+    if (name.length >= 17) {
+      return `${name.slice(0, 17)}...`;
+    }
+
+    return name;
+  }
+
+  const displayName = formatProductName(product.name);
+  const formatRating = product.averageRating.toFixed(1).replace(",", ".");
+
   return {
     product,
+    displayName,
+    formatRating,
   };
 }
