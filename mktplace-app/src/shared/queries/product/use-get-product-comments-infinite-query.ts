7c05345cb3f1868f41c88cd27e17bdd2e50f70ad
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2026-01-13 20:58:38 -0300

    creating infinite query for comments

---
+++ mktplace-app/src/shared/queries/product/use-get-product-comments-infinite-query.ts
@@ -0,0 +1,35 @@
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { buildImageURL } from "../../helpers/build-image-url";
+import { getProductComments } from "../../services/product.service";
+
+export function useGetProductCommentsInfiniteQuery(productId: number) {
+  const query = useInfiniteQuery({
+    queryFn: ({ pageParam = 1 }) =>
+      getProductComments({
+        productId,
+        pagination: { page: pageParam, perPage: 20 },
+      }),
+    queryKey: ["product-comments", productId],
+    getNextPageParam: (lastPage) => {
+      if (lastPage.page < lastPage.totalPages) {
+        return lastPage.page + 1;
+      }
+
+      return undefined;
+    },
+    initialPageParam: 1,
+  });
+
+  const comments =
+    query.data?.pages
+      .flatMap((page) => page.data)
+      .map((comment) => ({
+        ...comment,
+        user: {
+          ...comment.user,
+          avatar: buildImageURL(comment.user.avatar.url),
+        },
+      })) ?? [];
+
+  return { ...query, comments };
+}
