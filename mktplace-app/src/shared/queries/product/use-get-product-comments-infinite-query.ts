a723655216ed529495c6b72a772adc1751752f94
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2026-01-14 19:06:38 -0300

    creating comment component

--- mktplace-app/src/shared/queries/product/use-get-product-comments-infinite-query.ts
+++ mktplace-app/src/shared/queries/product/use-get-product-comments-infinite-query.ts
@@ -1,8 +1,9 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { buildImageURL } from "../../helpers/build-image-url";
+import { ProductComment } from "../../interfaces/product-comment";
 import { getProductComments } from "../../services/product.service";
 
-export function useGetProductCommentsInfiniteQuery(productId: number) {
+export const useGetProductCommentsInfiniteQuery = (productId: number) => {
   const query = useInfiniteQuery({
     queryFn: ({ pageParam = 1 }) =>
       getProductComments({
@@ -14,22 +15,26 @@ export function useGetProductCommentsInfiniteQuery(productId: number) {
       if (lastPage.page < lastPage.totalPages) {
         return lastPage.page + 1;
       }
-
       return undefined;
     },
     initialPageParam: 1,
   });
 
-  const comments =
+  const comments: ProductComment[] =
     query.data?.pages
       .flatMap((page) => page.data)
       .map((comment) => ({
         ...comment,
         user: {
           ...comment.user,
-          avatar: buildImageURL(comment.user.avatar?.url ?? ""),
+          avatar: {
+            url: buildImageURL(comment.user.avatar?.url ?? ""),
+          },
         },
       })) ?? [];
 
-  return { ...query, comments };
-}
+  return {
+    ...query,
+    comments,
+  };
+};
--- mktplace-app/src/view-models/product/product.view.tsx
+++ mktplace-app/src/view-models/product/product.view.tsx
@@ -1,6 +1,7 @@
 import { FlatList, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
+import { CommentItem } from "./components/comment-item";
 import { Header } from "./components/header";
 
 import { useProductModel } from "./userProduct.model";
@@ -27,7 +28,7 @@ export function ProductView({
     <SafeAreaView className="flex-1 bg-background">
       <FlatList
         data={comments}
-        renderItem={() => <></>}
+        renderItem={({ item }) => <CommentItem comment={item} />}
         ListHeaderComponent={() => <Header productDetails={productDetail} />}
         className="px-6"
       />
