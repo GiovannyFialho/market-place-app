929017b81fd95054d2bd7e2e476e5c66a65b7431
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2026-01-27 18:41:20 -0300

    creating services of comments

--- mktplace-app/src/shared/services/product.service.ts
+++ mktplace-app/src/shared/services/product.service.ts
@@ -1,9 +1,17 @@
 import { marketPlaceAPIClient } from "@/shared/api/market-place";
+import {
+  CreateCommentRequest,
+  CreateCommentResponse,
+} from "@/shared/interfaces/http/create-comment";
 
 import type { PaginatedResponse } from "@/shared/interfaces/http/paginated-response";
 import type { ProductRequest } from "@/shared/interfaces/http/product";
 import type { GetProductCommentsInterface } from "@/shared/interfaces/http/product-comments";
 import type { GetProductDetailsInterface } from "@/shared/interfaces/http/product-detail";
+import {
+  UpdateCommentRequest,
+  UpdateCommentResponse,
+} from "@/shared/interfaces/http/update-comment";
 import type {
   ProductCategory,
   ProductInterface,
@@ -40,5 +48,35 @@ export const getProductComments = async (
   const { data } = await marketPlaceAPIClient.post<
     PaginatedResponse<ProductComment>
   >("/products/comments", params);
+  return data;
+};
+
+export const createComment = async (params: CreateCommentRequest) => {
+  const { data } = await marketPlaceAPIClient.post<CreateCommentResponse>(
+    "/products/create/comments",
+    params,
+  );
+
+  return data;
+};
+
+export const getUserComment = async (productId: number) => {
+  const { data } = await marketPlaceAPIClient.get<{
+    content: string;
+    rating: number;
+  }>(`/products/${productId}/user-comment`);
+
+  return data;
+};
+
+export const updateUserComment = async (params: UpdateCommentRequest) => {
+  const { data } = await marketPlaceAPIClient.put<UpdateCommentResponse>(
+    `/products/comments/${params.commentId}`,
+    {
+      content: params.content,
+      rating: params.rating,
+    },
+  );
+
   return data;
 };
