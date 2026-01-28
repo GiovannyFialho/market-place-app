61a6fa03bb20025c956106d73c1e17d352636120
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2026-01-28 20:17:26 -0300

    creating product cart card

--- mktplace-app/src/view-models/cart/cart.view.tsx
+++ mktplace-app/src/view-models/cart/cart.view.tsx
@@ -1,7 +1,19 @@
-import { View } from "react-native";
+import { FlatList } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 import { useCartViewModel } from "@/view-models/cart/cart.model";
 
-export function CartView({}: ReturnType<typeof useCartViewModel>) {
-  return <View></View>;
+import { ProductCartCard } from "@/view-models/cart/components/product-cart-card";
+
+export function CartView({ products }: ReturnType<typeof useCartViewModel>) {
+  return (
+    <SafeAreaView>
+      <FlatList
+        data={products}
+        keyExtractor={({ id }) => `product-cart-id-${id}`}
+        contentContainerClassName="px-6"
+        renderItem={({ item }) => <ProductCartCard product={item} />}
+      />
+    </SafeAreaView>
+  );
 }
