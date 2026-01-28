723a7a7785484aa3672e14f6482a5a2cb35b8fee
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2026-01-28 20:53:21 -0300

    creating header and empty component

--- mktplace-app/src/view-models/cart/cart.view.tsx
+++ mktplace-app/src/view-models/cart/cart.view.tsx
@@ -3,6 +3,8 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 import { useCartViewModel } from "@/view-models/cart/cart.model";
 
+import { CartHeader } from "@/view-models/cart/components/cart-header";
+import { EmptyList } from "@/view-models/cart/components/empty-list";
 import { ProductCartCard } from "@/view-models/cart/components/product-cart-card";
 
 export function CartView({ products }: ReturnType<typeof useCartViewModel>) {
@@ -12,7 +14,9 @@ export function CartView({ products }: ReturnType<typeof useCartViewModel>) {
         data={products}
         keyExtractor={({ id }) => `product-cart-id-${id}`}
         contentContainerClassName="px-6"
+        ListHeaderComponent={<CartHeader />}
         renderItem={({ item }) => <ProductCartCard product={item} />}
+        ListEmptyComponent={<EmptyList />}
       />
     </SafeAreaView>
   );
---
+++ mktplace-app/src/view-models/cart/components/empty-list/index.tsx
@@ -0,0 +1,34 @@
+import { Ionicons } from "@expo/vector-icons";
+import { router } from "expo-router";
+import { Text, View } from "react-native";
+
+import { AppButton } from "@/shared/components/app-button";
+
+import { colors } from "@/styles/colors";
+
+export function EmptyList() {
+  return (
+    <>
+      <View className="flex-1 items-center px-20 pt-16">
+        <Ionicons name="cart-outline" size={36} color={colors.grays[200]} />
+
+        <Text className="text-sm font-bold text-gray-400 my-4">
+          Seu carrinho está vazio
+        </Text>
+
+        <Text className="text-sm text-gray-400 text-center mb-8">
+          Explore o catálogo de produtos e faça sua primeira compra!
+        </Text>
+      </View>
+
+      <AppButton
+        onPress={() => router.push("/(private)/(tabs)/home")}
+        leftIcon="storefront-outline"
+        variant="outlined"
+        className="w-[197px] self-center"
+      >
+        Explorar produtos
+      </AppButton>
+    </>
+  );
+}
