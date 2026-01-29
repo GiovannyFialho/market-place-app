6d4133741af76b0eaa7e8f9c870962055ce16d1f
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2026-01-28 21:01:12 -0300

    creating footer component

--- mktplace-app/src/view-models/cart/cart.view.tsx
+++ mktplace-app/src/view-models/cart/cart.view.tsx
@@ -3,6 +3,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 import { useCartViewModel } from "@/view-models/cart/cart.model";
 
+import { CartFooter } from "@/view-models/cart/components/cart-footer";
 import { CartHeader } from "@/view-models/cart/components/cart-header";
 import { EmptyList } from "@/view-models/cart/components/empty-list";
 import { ProductCartCard } from "@/view-models/cart/components/product-cart-card";
@@ -16,6 +17,7 @@ export function CartView({ products }: ReturnType<typeof useCartViewModel>) {
         contentContainerClassName="px-6"
         ListHeaderComponent={<CartHeader />}
         renderItem={({ item }) => <ProductCartCard product={item} />}
+        ListFooterComponent={<CartFooter />}
         ListEmptyComponent={<EmptyList />}
       />
     </SafeAreaView>
