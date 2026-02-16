840a47863cc366267f2f0b46c55d3eb2d6fa892a
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2026-02-16 14:49:14 -0300

    creating list-header component for orders

--- mktplace-app/src/view-models/orders/orders.view.tsx
+++ mktplace-app/src/view-models/orders/orders.view.tsx
@@ -2,18 +2,20 @@ import { FlatList } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import { EmptyList } from "@/view-models/orders/components/empty-list";
+import { ListHeader } from "@/view-models/orders/components/list-header";
 import { OrderItem } from "@/view-models/orders/components/order-item";
 
 import { useOrdersViewModel } from "@/view-models/orders/orders.model";
 
 export function OrdersView({ orders }: ReturnType<typeof useOrdersViewModel>) {
   return (
-    <SafeAreaView className="flex-1">
+    <SafeAreaView edges={["top"]} className="flex-1">
       <FlatList
         data={orders}
         keyExtractor={({ id }) => `order-${id}`}
+        ListHeaderComponent={ListHeader}
         renderItem={({ item: order }) => <OrderItem order={order} />}
-        ListEmptyComponent={<EmptyList />}
+        ListEmptyComponent={EmptyList}
         contentContainerClassName="px-[16px] pb-[120px]"
       />
     </SafeAreaView>
